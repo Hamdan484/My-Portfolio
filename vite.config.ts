6819ad306a59c67a9a190a77888b2f4690b905e3
Hamdan484 <207383911+Hamdan484@users.noreply.github.com.>
2026-09-09 12:40:47 +0000

    litle touches

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
   const emitSourcemaps = mode === 'development'
 
   return {
-    base:'/My-Portfolio/',
+    base: './',
     build: {
       sourcemap: emitSourcemaps ? 'inline' : false,
       minify: !emitSourcemaps,
